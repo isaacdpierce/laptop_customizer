d2764fcc289906f0648fc7241c6738e76b143063
Isaac Pierce <contact@isaacdpierce.com>
2019-05-13 09:33:14 -0700

    Converts all stateless components to functions

--- src/FeatureItem/FeatureItem.js
+++ src/FeatureItem/FeatureItem.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './FeatureItem.css';
 
-export default class Features extends Component {
-  render() {
-    const {
-      newValue,
-      index,
-      feature,
-      featureClass,
-      updateFeature,
-    } = this.props;
+const FeatureItem = ({
+  newValue,
+  index,
+  feature,
+  featureClass,
+  updateFeature,
+}) => (
+  <li key={index} className='feature__item'>
+    <div
+      className={featureClass}
+      onClick={e => updateFeature(feature, newValue)}
+    >
+      {newValue.name}(
+      {new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(newValue.cost)}
+      )
+    </div>
+  </li>
+);
 
-    return (
-      <li key={index} className='feature__item'>
-        <div
-          className={featureClass}
-          onClick={e => updateFeature(feature, newValue)}
-        >
-          {newValue.name}(
-          {new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-          }).format(newValue.cost)}
-          )
-        </div>
-      </li>
-    );
-  }
-}
+export default FeatureItem;
--- src/SummaryOption/SummaryOption.js
+++ src/SummaryOption/SummaryOption.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const SummaryOption = ({ value, name, cost }) => {
-  return (
-    <div className='summary__option' key={value}>
-      <div className='summary__option__label'>{value} </div>
-      <div className='summary__option__value'>{name}</div>
-      <div className='summary__option__cost'>
-        {new Intl.NumberFormat('en-US', {
-          style: 'currency',
-          currency: 'USD',
-        }).format(cost)}
-      </div>
+const SummaryOption = ({ value, name, cost }) => (
+  <div className='summary__option' key={value}>
+    <div className='summary__option__label'>{value} </div>
+    <div className='summary__option__value'>{name}</div>
+    <div className='summary__option__cost'>
+      {new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(cost)}
     </div>
-  );
-};
+  </div>
+);
 
 export default SummaryOption;
